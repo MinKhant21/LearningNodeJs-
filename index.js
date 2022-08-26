a90c59113295db412b0b546c9e4cc654f0542298
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-08-26 08:54:51 +0630

    Async

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 console.log('start');
-const note = fs.readFileSync('notes.txt');
-console.log(note.toString());
+// const note = fs.readFileSync('notes.txt');
+// console.log(note.toString());
+
+fs.readFile('notes.txt',(err,data)=>{
+    if(err) throw err;
+    console.log(data.toString())
+});
+
 console.log('end');
