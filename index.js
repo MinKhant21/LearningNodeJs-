72674a61dcdd776b86cc677c200909595d2ec491
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-08-29 21:28:17 +0630

    formdata

--- index.js
+++ index.js
@@ -2,20 +2,37 @@
 
 const http = require('http');
 const fs = require('fs');
+const qs = require('querystring');
 
 http.createServer((req,res)=>{
     const url = req.url;
+    const method = req.method;
+     
+    if(url == "/" && method=="POST"){
+        let data = "";
+        req.on('data',d=>{
+            data+=d;
+        })
+
+        req.on('end',()=>{
+            console.log(qs.parse(data));
+        })
+    }
     if(url == "/"){
         fs.readFile('./page/index.html',(err,data)=>{
-            res.end(data);   
+            res.write(data)
+            res.end();
+              
             })
     }else if(url=="/about"){
         fs.readFile('./page/about.html',(err,data)=>{
-            res.end(data)
+            res.write(data)
+            res.end();
         })
     }else if(url=="/contact"){
         fs.readFile('./page/contact.html',(err,data)=>{
-            res.end(data)
+            res.write(data)
+            res.end();
         })
     }
     
